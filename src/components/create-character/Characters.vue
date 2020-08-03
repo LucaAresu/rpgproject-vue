f52882771fcd4efe64d35cb6e9b0f3180792f38c
Luca Aresu <luca.aresu90@gmail.com>
2020-08-03 10:10:45 +0200

    add animation on switch character

--- src/components/create-character/Characters.vue
+++ src/components/create-character/Characters.vue
@@ -1,47 +1,64 @@
 <template>
-	<div>
-		<div class="char">
-			<div class="arrow">
-				<button @click="$emit('changeChar',-1)">Precedente</button>
-			</div>
-			<div>
-				<img :src="require('@/assets/characters/'+index+'.png')">
-			</div>
-			<div class="arrow">
-				<button @click="$emit('changeChar',1)"> Successivo</button>
-			</div>
-		</div>
-	</div>
+  <div>
+    <div class="char">
+      <div class="arrow">
+        <button @click="$emit('changeChar',-1)">Precedente</button>
+      </div>
+      <div class="image">
+        <transition name="fade" mode="out-in">
+          <img :src="require('@/assets/characters/'+index+'.png')" :key="index"/>
+        </transition>
+      </div>
+      <div class="arrow">
+        <button @click="$emit('changeChar',1)">Successivo</button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-	export default {
-		props : ['index'],
-	};
+export default {
+  props: ['index']
+}
 </script>
 
 <style scoped>
 .char {
-	display: flex;
-	flex-direction: row;
-	justify-content: space-around;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
 .arrow {
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 img {
-	height: 300px;
-	width: 300px;
+  height: 300px;
+  width: 300px;
 }
 .arrow button {
-	padding: 1rem;
-	background-color: #ccc;
-	border: none;
-	box-shadow: 1px 1px 2px black;
-	height: auto;
+  padding: 1rem;
+  background-color: #ccc;
+  border: none;
+  box-shadow: 1px 1px 2px black;
+  height: auto;
 }
 button:hover {
-	box-shadow: inset 1px 1px 2px black;
+  box-shadow: inset 1px 1px 2px black;
+}
+
+.fade-enter-active {
+  animation: fade-in 200ms ease-out forwards;
+}
+@keyframes fade-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+.image {
+  overflow: hidden;
 }
 </style>
--- src/components/create-character/CreateCharacters.vue
+++ src/components/create-character/CreateCharacters.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <h1>Crea il tuo personaggio </h1>
-    <characters :index="index" @changeChar="index = ((total + index + $event)%total)" />
+      <characters :index="index" @changeChar="index = ((total + index + $event)%total)" />
     <name @complete="createCharacter($event)" />
   </div>
 </template>
 <script>
-import constants from '../../constants';
-import Characters from './Characters';
-import Name from './ChooseName';
+import constants from '../../constants'
+import Characters from './Characters'
+import Name from './ChooseName'
 export default {
-  data() {
+  data () {
     return {
-      index : 0,
-      total : constants.totalCharacters,
+      index: 0,
+      total: constants.totalCharacters
     }
   },
   components: {
     Characters,
-    Name,
+    Name
   },
   methods: {
-    createCharacter(name) {
+    createCharacter (name) {
       this.$store.dispatch('createCharacter', {
         name,
-        avatar : this.index
-      });
-    },
+        avatar: this.index
+      })
+    }
   }
 }
 </script>
