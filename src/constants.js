3a074949c4ba06bdcb3d0da3f7cb6016f6e3571b
LucaAresu <luca.aresu90@gmail.com>
2020-08-08 22:19:04 +0200

    add error

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,7 @@ export default {
     EMAIL_EXISTS: 'L\'Email è già presente',
     INVALID_EMAIL: 'Email non valida',
     MISSING_PASSWORD: 'Password Mancante',
-    EMAIL_NOT_FOUND: 'Email non trovata'
+    EMAIL_NOT_FOUND: 'Email non trovata',
+    INVALID_PASSWORD: 'Password Invalida'
   }
 }
