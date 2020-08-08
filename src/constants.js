7e30d6d67cdc0d8550b0ec5e660978c9e5dce645
LucaAresu <luca.aresu90@gmail.com>
2020-08-08 22:13:54 +0200

    add errors

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,8 @@ export default {
   errors: {
     RISPOSTA_INVALIDA: 'E\' stata ricevuta una risposta invalida',
     EMAIL_EXISTS: 'L\'Email è già presente',
-    INVALID_EMAIL: 'Email non valida'
+    INVALID_EMAIL: 'Email non valida',
+    MISSING_PASSWORD: 'Password Mancante',
+    EMAIL_NOT_FOUND: 'Email non trovata'
   }
 }
